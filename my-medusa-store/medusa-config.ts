22f2607d05f855f339e8b4b235d6672f1453d6b8
Mark Munyaka <markmunyaka@gmail.com>
2024-10-31 14:23:45 +0000

    Minor fix

--- my-medusa-store/medusa-config.ts
+++ my-medusa-store/medusa-config.ts
@@ -1,10 +1,17 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+// 3. import Modules
+import { loadEnv, defineConfig, Modules } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+  admin: {
+    // 1. Add Medusa Backend URL
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+        // 2. Add Redis URL
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -12,5 +19,28 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  }
+  },
+  // 4. Add Modules
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+  ]
 })
